13fe4356d1701dc80e59df366c0d37d85ce55e7c
luvagu <luiavag@gmail.com>
2021-04-28 02:56:58 -0300

    added a layout component

---
+++ components/Layout.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import { Loader, Navbar } from '.'
+
+function Layout({ children }) {
+	const [isLoading, setIsLoading] = useState(true)
+
+	return (
+		<div id="rootDiv" className="min-h-screen grid">
+			{isLoading ? (
+				<Loader finishLoading={() => setIsLoading(false)} />
+			) : (
+				<div className="flex flex-col min-h-screen">
+					<Navbar />
+					<div id="content">
+                        {children}
+					</div>
+				</div>
+			)}
+            <style jsx>{`
+                #rootDiv {
+                    grid-template-rows: 1fr auto;
+                    grid-template-columns: 100%;
+                }
+            `}</style>
+		</div>
+	)
+}
+
+export default Layout
--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
+export { default as Layout } from './Layout'
 export { default as Loader } from './Loader'
 export { default as Navbar } from './Navbar'
 export { default as MobileMenu } from './MobileMenu'
