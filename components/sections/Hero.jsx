58ba7988a06b22dffd4a0711b12fe5140262598e
luvagu <luiavag@gmail.com>
2021-04-29 18:12:51 -0300

    added hero section

--- components/sections/Hero.jsx
+++ components/sections/Hero.jsx
@@ -1,8 +1,22 @@
 function Hero() {
     return (
-        <div>
-            
-        </div>
+        <section className="flex justify-center items-start flex-col min-h-screen p-0">
+            <h1 className="mt-0 mr-0 mb-7 ml-1"></h1>
+
+            <h3></h3>
+
+            <style jsx>{`
+                @media (max-width: 480px) {
+                    section {
+                        padding-bottom: 10vh;
+                    }
+
+                    section > h1 {
+                        margin: 0 0 20px 2px;
+                    }
+                }
+            `}</style>
+        </section>
     )
 }
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,11 @@
-import { Layout } from '../components'
+import { Layout, Hero } from '../components'
 
 export default function Home() {
 	return (
 		<Layout isHome={true}>
 			{/* className="my-0 mx-auto min-h-screen max-w-screen-2xl py-32 px-6 sm:py-36 sm:px-12 md:py-52 md:px-24 lg:py-52 lg:px-36" */}
 			<main className="my-0 mx-auto min-h-screen max-w-screen-2xl py-0 px-6 sm:px-12 md:px-24 lg:px-36">
-				<div className="text-5xl text-white min-h-screen">Here Goes The Content</div>
+				<Hero />
 			</main>
 		</Layout>
 	)
