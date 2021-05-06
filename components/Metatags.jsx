7b25c97e30080fe3f7d792b65e12704af436000c
luvagu <luiavag@gmail.com>
2021-05-06 00:41:44 -0300

    added favicons

--- components/Metatags.jsx
+++ components/Metatags.jsx
@@ -17,6 +17,11 @@ function Metatags({
 			<meta property="og:title" content={title} />
 			<meta property="og:description" content={description} />
 			<meta property="og:image" content={imageUrl} />
+
+			<link rel="icon" href="/favicon.ico" />
+			<link rel="apple-touch-icon" href="/img/apple-touch-icon.png" />
+			<link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png" />
+			<link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png" />
 		</Head>
 	)
 }
