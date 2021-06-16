817cc848a2ae4b49a69003f3b27151f53aaa1188
luvagu <luiavag@gmail.com>
2021-06-16 18:32:22 -0500

    modified analytics implementation

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,31 +1,25 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from '../utils/gtag'
 
 class MyDocument extends Document {
-	// static async getInitialProps(ctx) {
-	// 	const initialProps = await Document.getInitialProps(ctx)
-	// 	return { ...initialProps }
-	// }
-
 	render() {
 		return (
 			<Html lang='en'>
 				<Head>
 					<script
 						async
-						src='https://www.googletagmanager.com/gtag/js?id=G-94QPTMT7G3'
+						src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
 					/>
 					<script
 						dangerouslySetInnerHTML={{
 							__html: `
-						window.dataLayer = window.dataLayer || []
-						function gtag() {
-							dataLayer.push(arguments)
-						}
-						gtag('js', new Date())
-
-						gtag('config', 'G-94QPTMT7G3')
-						console.log('loaded analytics G-94QPTMT7G3')
-						`,
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
 						}}
 					/>
 				</Head>
