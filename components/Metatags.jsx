dfbe0076615827ffca3d793c55638aac8ac9e247
luvagu <luiavag@gmail.com>
2021-06-16 19:15:24 -0500

    removed google analytics

--- components/Metatags.jsx
+++ components/Metatags.jsx
@@ -8,23 +8,33 @@ function Metatags({
 	return (
 		<Head>
 			<title>{title}</title>
-			<meta name="description" content={description} />
-			<meta name="lang" content={description} />
+			<meta name='description' content={description} />
+			<meta name='lang' content={description} />
 
-			<meta name="twitter:card" content="summary" />
-			<meta name="twitter:site" content="@luiavag" />
-			<meta name="twitter:title" content={title} />
-			<meta name="twitter:description" content={description} />
-			<meta name="twitter:image" content={imageUrl} />
+			<meta name='twitter:card' content='summary' />
+			<meta name='twitter:site' content='@luiavag' />
+			<meta name='twitter:title' content={title} />
+			<meta name='twitter:description' content={description} />
+			<meta name='twitter:image' content={imageUrl} />
 
-			<meta property="og:title" content={title} />
-			<meta property="og:description" content={description} />
-			<meta property="og:image" content={imageUrl} />
+			<meta property='og:title' content={title} />
+			<meta property='og:description' content={description} />
+			<meta property='og:image' content={imageUrl} />
 
-			<link rel="icon" href="/favicon.ico" />
-			<link rel="apple-touch-icon" href="/img/apple-touch-icon.png" />
-			<link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png" />
-			<link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png" />
+			<link rel='icon' href='/favicon.ico' />
+			<link rel='apple-touch-icon' href='/img/apple-touch-icon.png' />
+			<link
+				rel='icon'
+				type='image/png'
+				sizes='32x32'
+				href='/img/favicon-32x32.png'
+			/>
+			<link
+				rel='icon'
+				type='image/png'
+				sizes='16x16'
+				href='/img/favicon-16x16.png'
+			/>
 		</Head>
 	)
 }
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,27 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID
-
 class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang='en'>
-				<Head>
-					<script
-						async
-						src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-					/>
-					<script
-						dangerouslySetInnerHTML={{
-							__html: `
-								window.dataLayer = window.dataLayer || [];
-								function gtag(){dataLayer.push(arguments);}
-								gtag('js', new Date());
-								gtag('config', '${GA_TRACKING_ID}');
-							`,
-						}}
-					/>
-				</Head>
+				<Head />
 				<body>
 					<Main />
 					<NextScript />
